e267833f7c527a769c6917694934df7c9dd8b5d8
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2023-07-06 11:20:58 +0300

    Add files via upload

--- index.script.js
+++ index.script.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", () => {
+  loadTasksFromLocalStorage();
+});
+
+
 class TaskBlock {
     constructor(value) {
       this.value = value;
@@ -14,9 +19,11 @@ class TaskBlock {
   
       const doneButton = new DoneButton();
       const removeButton = new RemoveButton();
+      const editButton = new EditButton();
   
       newBlock.appendChild(newText);
       newBlock.appendChild(doneButton.element);
+      newBlock.appendChild(editButton.element);
       newBlock.appendChild(removeButton.element);
   
       return newBlock;
@@ -66,31 +73,64 @@ class RemoveButton {
       componentDiv.parentNode.removeChild(componentDiv);
     }
 }
+
+class EditButton {
+  constructor() {
+    this.element = this.createButton();
+  }
+
+  createButton() {
+    const button = document.createElement("button");
+    button.className = "flex-no-shrink p-2 ml-2 border-2 rounded text-blue border-blue hover:text-white hover:bg-blue";
+    button.innerHTML = "Edit";
+    button.addEventListener("click", () => this.editTask());
+    return button;
+  }
+
+  editTask() {
+    const paragraph = this.element.parentNode.querySelector("p");
+    const newText = prompt("Введите новый текст задачи:", paragraph.innerText);
+    if (newText !== null) {
+      paragraph.innerText = newText;
+      saveTasksToLocalStorage();
+    }
+  }
+}
   
 function insertValue() {
-    const input = document.getElementById("ToDoInput");
-    const value = input.value;
+  const input = document.getElementById("ToDoInput");
+  const value = input.value;
+  if(value == ""){
+    alert("Вы не ввели задачу!")
+  }else{
     const newTask = new TaskBlock(value);
-  
+
     const resultContainer = document.getElementById("resultContainer");
     resultContainer.appendChild(newTask.element);
-  }
-  
-function markDone(button) {
-    const paragraph = button.parentNode.querySelector("p");
-    if (paragraph.classList.contains("line-through")) {
-      paragraph.classList.remove("line-through");
-      button.innerHTML = "Not Done";
-    } else {
-      paragraph.classList.add("line-through");
-      button.innerHTML = "Done";
-    }
-}
-  
-function removeComponent(button) {
-    const componentDiv = button.parentNode;
-    componentDiv.parentNode.removeChild(componentDiv);
-}
-  
 
-  
+    saveTasksToLocalStorage();
+  }
+}
+
+function saveTasksToLocalStorage() {
+  const taskElements = document.querySelectorAll(".flex.mb-4.items-center");
+  const tasks = [];
+  taskElements.forEach((element) => {
+    const text = element.querySelector("p").innerText;
+    tasks.push(text);
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasksFromLocalStorage() {
+  const savedTasks = localStorage.getItem("tasks");
+  if (savedTasks) {
+    const tasks = JSON.parse(savedTasks);
+    tasks.forEach((task) => {
+      const newTask = new TaskBlock(task);
+      const resultContainer = document.getElementById("resultContainer");
+      resultContainer.appendChild(newTask.element);
+    });
+  }
+}
